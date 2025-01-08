69e494360e3484f04ec7da0df4d4cadcd09bc7bd
ZEDPortugal <123491971+ZEDPortugal@users.noreply.github.com>
2025-01-08 16:06:52 +0800

    Update App.jsx

--- components/App.jsx
+++ components/App.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import Navbar from './Navbar';
 import Skills from './Skills';
 import About from './About';
 import Projects from './Projects';
+import Home from './Home';
+import ChatBot from './ChatBot';
 
 const App = () => {
   const [isLight, setIsLight] = useState(false);
@@ -12,9 +15,12 @@ const App = () => {
 
   return (
     <div>
+      <Navbar isLight={isLight} handleToggle={handleToggle} />
+      <Home isLight={isLight} handleToggle={handleToggle} />
       <Skills isLight={isLight} handleToggle={handleToggle} />
       <About isLight={isLight} />
       <Projects isLight={isLight} />
+      <ChatBot isLight={isLight} />
     </div>
   );
 };
