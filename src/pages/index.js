b7ced66aca9da5e49c750aec87da5372cc5baa2a
ZEDPortugal <123491971+ZEDPortugal@users.noreply.github.com>
2025-01-08 16:56:33 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
+import About from "../../components/About";
+import ChatBot from "../../components/chatbot";
+import Home from "../../components/Home";
+import Navbar from "../../components/Navbar";
+import Projects from "../../components/Projects";
+import Skills from "../../components/Skills";
 import App from "../../components/App";
 
 export default function HomePage() {
