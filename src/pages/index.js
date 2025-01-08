b634b566902b89b5ab83cb8803906f96c3fe40f0
ZEDPortugal <123491971+ZEDPortugal@users.noreply.github.com>
2025-01-08 16:08:07 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,8 @@
-import Image from "next/image";
+
 import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from "../../components/Navbar";
-import Home from "../../components/Home";
-import Skills from "../../components/Skills";
-import About from "../../components/About";
+
 import App from "../../components/App";
-import ChatBot from "../../components/chatbot";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,9 +18,8 @@ export default function HomePage() {
   return (
     <>
  
-    <Home />
+  
     <App />   
-    <ChatBot />
     </>
 
   );
